d4f8a9d9c69573bc03811e3e2c030110a3cbe492
RommieRene <renegiragosyan@gmail.com>
2025-09-07 14:03:42 +0400

    push on rene tutor file

--- codritive/core/urls.py
+++ codritive/core/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('contact/', views.contact_us, name='contact_us'),
     path('get-in-touch/', views.get_in_touch, name='get_in_touch'),
     path('service/', views.service, name='service'),
+    path("tutor/<int:pk>/", views.tutor_detail, name="tutor"),
+
 ]
